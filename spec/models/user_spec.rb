3991a8c263dc2897a18d470d9ce40dd739b3686f
tomichihi <tsshc820@gmail.com>
2021-05-14 15:24:38 +0900

    テストコード微調整

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
- context '新規登録できるとき' do
-  describe 'ユーザー新規登録' do
+ describe 'ユーザー新規登録' do
+  context '新規登録できるとき' do
     it '項目が全て適切なら登録できる' do
       expect(@user).to be_valid
     end
@@ -16,47 +16,69 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
   end
- end
 
- context '新規登録できないとき' do
-  it 'nicknameが空では登録できない' do
+  context '新規登録できないとき' do
+    
+    it 'nicknameが空では登録できない' do
      @user.name = ''
      @user.valid?
      expect(@user.errors.full_messages).to include("Name can't be blank")
-  end
-  it 'emailが空では登録できない' do
+    end
+
+    it 'emailが空では登録できない' do
      @user.email =''
      @user.valid?
      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
 
-  end
-  it 'passwordが空では登録できない' do
+    it 'passwordが空では登録できない' do
      @user.password =''
      @user.valid?
      expect(@user.errors.full_messages).to include("Password can't be blank")
-  end
-  it 'passwordが存在してもpassword_confirmationが空では登録できない' do
-    @user.password_confirmation = ''
-    @user.valid?
+    end
+
+    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+     @user.password_confirmation = ''
+     @user.valid?
      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-  end
-  it 'nicknameが9文字以上では登録できない' do
+    end
+
+    it 'nameが9文字以上では登録できない' do
      @user.name = 'tomichihi'
      @user.valid?
      expect(@user.errors.full_messages).to include('Name is too long (maximum is 8 characters)')
-  end
-  it '重複したemailが存在する場合登録できない' do
+    end
+
+    it '重複したemailが存在する場合登録できない' do
      @user.save
      another_user = FactoryBot.build(:user)
      another_user.email = @user.email
      another_user.valid?
      expect(another_user.errors.full_messages).to include('Email has already been taken')
-  end
-  it 'passwordが5文字以下では登録できない' do
-    @user.password = '00000'
-    @user.password_confirmation = '00000'
-    @user.valid?
-    expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
+    it 'passwordが5文字以下では登録できない' do
+     @user.password = '00000'
+     @user.password_confirmation = '00000'
+     @user.valid?
+     expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
+    it 'passwordに英字が含まれていなければ登録できない'do
+     @user.password = '000000'
+     @user.password_confirmation = '00000'
+     @user.valid?
+     expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+
+    it 'passwordに数字が含まれていなければ登録できない'do
+     @user.password = 'tomita'
+     @user.password_confirmation = 'tomita'
+     @user.valid?
+     expect(@user.errors.full_messages).to include('Password is invalid')
+    end
   end
  end
 end
+
+ 
