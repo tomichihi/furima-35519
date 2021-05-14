c142fc1798e8982e56c6d721e41a5071cd3ea995
tomichihi <tsshc820@gmail.com>
2021-05-14 09:42:14 +0900

    新規登録、ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name, presence: true 
+         validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}     
 end
