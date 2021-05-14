55f1656043e2e8c09155a117648886d3a8b7555c
tomichihi <tsshc820@gmail.com>
2021-05-14 21:45:45 +0900

    微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         namae = /\A[ぁ-んァ-ン一-龥々ー]+\z/
+         kana = /\A[ァ-ヶー－]+\z/
+
     with_options presence: true do
          validates :name, length: { maximum: 8 }
          validates :password
          validates :password_confirmation
-         validates :last_name,format:{with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :first_name,format:{with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :last_name_kana,format:{with: /\A[ァ-ヶー－]+\z/}
-         validates :first_name_kana,format:{with: /\A[ァ-ヶー－]+\z/}
+         validates :last_name,format:{with: namae}
+         validates :first_name,format:{with: namae}
+         validates :last_name_kana,format:{with: kana}
+         validates :first_name_kana,format:{with: kana}
          validates :birth
     end 
          validates :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}   
