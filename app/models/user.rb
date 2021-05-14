515a0af4c850544257b0d327428fd4628731ce8c
tomichihi <tsshc820@gmail.com>
2021-05-14 14:06:53 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :name, presence: true 
-         validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}     
+         validates :name,                 presence: true, length: { maximum: 8 }
+         validates :password,             presence: true
+         validates :password_confirmation,presence: true
+         validates :encrypted_password, :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}  
+         validates :last_name,            presence: true
+         validates :first_name,           presence: true
+         validates :last_name_kana,       presence: true
+         validates :first_name_kana,      presence: true
+         validates :birth,                presence: true   
 end
