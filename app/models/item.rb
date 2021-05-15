41ed12a8ade3c4df4110c5cd1685445eeaf186c3
tomichihi <tsshc820@gmail.com>
2021-05-15 19:41:49 +0900

    プルダウンメニュー

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :days
+  belongs_to :fee
   has_one :record
   belongs_to :user
-  
+
+  with_options numericality: { other_than: 0 } do
+  validates :area_id
+  validates :category_id
+  validates :condition_id
+  validates :days_id
+  validates :fee_id
+  end
+
   with_options presence: true do
   validates :title
   validates :price
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_042638) do
+ActiveRecord::Schema.define(version: 2021_05_15_075652) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
