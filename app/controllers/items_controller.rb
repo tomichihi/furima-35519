8a5a44333c13bee1c0212eaec459ef290b071ef9
tomichihi <tsshc820@gmail.com>
2021-05-18 16:06:29 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :user_validation, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :user_validation, only: [:edit, :update ,:destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
    end
    
   end
+
+  def destroy
+     @item.destroy
+     redirect_to root_path 
+  end  
 
   private
   def set_item
