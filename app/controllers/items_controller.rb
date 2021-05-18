9e47a4f15593ae6b11aa16d327fe890fba7da337
tomichihi <tsshc820@gmail.com>
2021-05-18 15:35:21 +0900

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :user_validation, only: [:edit, :update]
 
@@ -22,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])   
   end
 
   def update 
-   @item = Item.find(params[:id])
 
    if @item.update(item_params)
      redirect_to item_path
