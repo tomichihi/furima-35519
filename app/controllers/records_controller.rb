5cf00bc586518ae6d5cc06509946727be3b71e1c
tomichihi <tsshc820@gmail.com>
2021-05-22 16:47:28 +0900

    微調整

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,13 +2,13 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :set_item, only: [:index,:create]
   before_action :validation, only: [:index]
-  
-  def index
-    @record_place = RecordPlace.new
-    render 'records/index'
-  end
-
-  def create
+                                                   
+  def index                                  
+    @record_place = RecordPlace.new             
+    render 'records/index'                      
+  end                           
+                      
+  def create              
     @record_place = RecordPlace.new(place_params)
 
     if @record_place.valid?
@@ -28,7 +28,7 @@ class RecordsController < ApplicationController
   def place_params
     params.require(:record_place).permit(:pos_code, :area_id, :city, :address, :building, :phone_num).merge(token: params[:token],price: @item.price, item_id: @item.id, user_id:current_user.id)
   end
-
+  
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
@@ -37,7 +37,7 @@ class RecordsController < ApplicationController
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end 
-
+  
   def validation
     if @item.user_id == current_user.id || @item.record != nil
     redirect_to root_path
@@ -45,8 +45,9 @@ class RecordsController < ApplicationController
   end
   
   
-
   
-
-
+   
+  
+  
 end
+  
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,6 @@ const pay = () => {
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     
-    console.log(111)
     e.preventDefault();
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
@@ -14,16 +13,13 @@ const pay = () => {
       exp_month: formData.get("record_place[exp_month]"),
       exp_year: `20${formData.get("record_place[exp_year]")}`
     };
-
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token);
-    debugger
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
+      };
 
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
